ba264d30a0899738fe6fe4d7898703ed8a7c5faa
Mr Rohan <palrohan1235@gmail.com>
2024-02-21 22:31:50 +0530

    added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import { Outlet } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function Layout() {
     return(
         <>
+        <HashRouter>
           <Header/>
            <Outlet/>
           <Footer/>
+          </HashRouter>
         </>
     )
     
